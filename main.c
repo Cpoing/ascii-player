eac29766877f68b33d0428802ea8ce453f3eeb99
Ted Lee <tedlee868@gmail.com>
2026-06-17 22:26:34 -0400

    Added frame buffering

--- main.c
+++ main.c
@@ -21,25 +21,26 @@ int main()
 
   FILE *pipe = popen(cmd, "r");
   if (!pipe) { perror("popen"); return 1;}
-  //fscanf(f, "%2s %d %d %d", magic, &w, &h, &maxval);
   
   unsigned char *frame = malloc(w * h * 3);
 
   printf("\x1b[?25l");
 
+  char *out = malloc(w * h + h + 32);
   while (fread(frame, 1, w * h * 3, pipe) == w * h * 3) {
-    printf("\x1b[H");
+    int pos = 0;
+    out[pos++] = '\x1b'; out[pos++] = '['; out[pos++] = 'H';
     for (int y = 0; y < h; y++) {
       for (int x = 0; x < w; x++) {
         unsigned char *px = &frame[(y * w + x) * 3];
-        //int lum = (int)(0.2126*px[0] + 0.7152*px[1] + 0.0722*px[2]);
         int lum = (int)(0.299*px[0] + 0.587*px[1] + 0.114*px[2]);
-        putchar(ASCII_RAMP[lum * ASCII_RAMP_LEN / 256]);
+        out[pos++] = ASCII_RAMP[lum * (ASCII_RAMP_LEN - 1) / 255];
       }
-      putchar('\n');
+      out[pos++] = '\n';
     }
+    fwrite(out, 1, pos, stdout);
+    fflush(stdout);
   }
-  printf("\x1b[?25h");
   pclose(pipe);
   free(frame);
   return 0;
